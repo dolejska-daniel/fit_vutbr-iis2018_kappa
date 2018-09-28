5e0360da75d604163065ac35a6e557478eb73f0f
Daniel Dolejška <xdolej08@stud.fit.vutbr.cz>
2018-09-28 19:55:49 +0200

    Základní struktura endpointů pro HTTP GET a POST požadavky

---
+++ src/GET/VeciEndpoint.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GET;
+
+use App\Authorizator;
+use App\StaticEndpoint;
+
+
+class VeciEndpoint extends StaticEndpoint
+{
+	public static function processRequest(): array
+	{
+		return [];
+	}
+
+	public static $resources = [
+		__CLASS__
+	];
+	public static function exportResources(): array { return self::$resources; }
+
+	public static $permissions = [
+		"allow" => [
+			Authorizator::ROLE_USER,
+		],
+		"deny" => [],
+	];
+	public static function exportPermissions(): array { return self::$permissions; }
+}
--- src/StaticEndpoint.php
+++ src/StaticEndpoint.php
@@ -2,14 +2,24 @@
 
 namespace App;
 
+use Nette\Database\Context;
+
 abstract class StaticEndpoint implements IEndpoint
 {
 	/** @var Service */
 	public static $app;
 
+	/** @var Context */
+	public static $db;
+
+	/**
+	 * @param Service $app
+	 * @return string
+	 */
 	public static function init( Service $app ): string
 	{
 		self::$app = $app;
+		self::$db = $app->context;
 		return self::class;
 	}
 }
